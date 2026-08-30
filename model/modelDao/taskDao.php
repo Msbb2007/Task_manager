85bc831e5505bf537de82c1298dce7d0b1ac433f
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-08-30 20:11:10 +0330

    add functions to taskDao.php

--- model/modelDao/taskDao.php
+++ model/modelDao/taskDao.php
@@ -2,6 +2,27 @@
 
 namespace model\modelDao;
 
+use model\mainClasses\tasks;
+
 class taskDao{
-    
+    public function createTask(tasks $tasks){
+
+    }
+    public function updateTask(tasks $tasks){
+
+    }
+    public function deleteTask(tasks $tasks){
+
+    }
+    public function getTaskById(int $id){
+
+    }
+
+    public function getTaskByUserId(int $userId){
+
+    }
+    public function getTasksByStatus(int $taskId){
+        
+    }
+
 }
