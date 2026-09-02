e29eb22a915b25347e4a7f5e7528c4cc658441ee
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-02 14:33:33 +0330

    edit process

--- view/process_auth.php
+++ view/process_auth.php
@@ -4,6 +4,9 @@ session_start();
 require_once '../model/db/connection.php';
 require_once '../model/modelDao/UserDao.php';
 require_once '../controller/AuthController.php';
+require_once '../model/mainClasses/users.php';
+require_once '../model/mainClasses/role.php';
+
 
 use model\modelDao\UserDao;
 use controller\Authcontroller\AuthController;
@@ -27,26 +30,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         
         $error = $authController->login($username, $password);
 
-        if ($error === null) {
-            header('Location:');
+        if ($error === 'admin') {
+            header('Location:adminView.php');
+            exit();
+        }elseif($error === 'member'){
+            header('Location:memberView.php');
             exit();
         }
 
     } elseif ($action === 'register') {
         $name = trim($_POST['name']);
-        $phone = trim($_POST['phone']);
+        $email = trim($_POST['email']);
         $password = $_POST['password'];
 
-        $error = $authController->register($name, $phone, $password);
+        $error = $authController->register($name, $email, $password);
 
-        if ($error === null) {
-            $_SESSION['success_message'] = "ثبت نام شما با موفقیت انجام شد. لطفاً وارد شوید.";
+        if ($error === "ثبت نام انجام شد") {
+            $_SESSION['success_message'] = "ثبت نام شما با موفقیت انجام شد. لطفاً وارد شوید";
+            header('Location: index.php'); 
+            exit();
+        }else{
+            $_SESSION['error_message'] = " این نام کاربری قبلا ثبت شده است";
             header('Location: index.php'); 
             exit();
         }
     }
 
-    if ($error !== null) {
+    if ($error === 'نام کاربری یا رمز عبور اشتباه است') {
         $_SESSION['error_message'] = $error;
         header('Location: index.php');
         exit();
