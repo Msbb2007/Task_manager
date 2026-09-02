b75d14f2dc7f06c3ba8af3669412284b808c8a94
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-02 13:11:17 +0330

    edit connection.php

--- model/db/connection.php
+++ model/db/connection.php
@@ -4,27 +4,32 @@ namespace model\db;
 
 use PDO;
 use PDOException;
-class connector{
+
+class connector {
     private static $host = "127.0.0.1";
     private static $port = "3307"; 
     private static $username = "root";
     private static $dbname = "taskdb"; 
 
     public static function getConnection() {
-        return new PDO("mysql:host=" . self::$host . ";port=" . self::$port , self::$username,"");
+        $dsn = "mysql:host=" . self::$host . ";port=" . self::$port . ";dbname=" . self::$dbname . ";charset=utf8mb4";
+        return new PDO($dsn, self::$username, "");
+    }
+
+    public static function getBaseConnection() {
+        $dsn = "mysql:host=" . self::$host . ";port=" . self::$port;
+        return new PDO($dsn, self::$username, "");
     }
 
     public static function createDb() {
         try {
-            $conn = self::getConnection();
+            $conn = self::getBaseConnection();
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
             $sql = "CREATE DATABASE IF NOT EXISTS " . self::$dbname;
             $conn->exec($sql);
-
         } catch(PDOException $e) {
             die("Error: " . $e->getMessage());
         }
     }
 }
-?>
